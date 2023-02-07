5f075daa4c0bfc8945b835a0bf109f488e794665
AswathiMohan23 <achu999here@gmail.com>
2023-02-07 10:53:07 +0530

    [ASWATHI] ADDED . code to add dynamic templates

--- calc/views.py
+++ calc/views.py
@@ -5,5 +5,5 @@ from django.shortcuts import render
 
 def home(request):
    # return HttpResponse("Hello World")
-   return render(request,'home.html') # render is used to call html page because we need to render the template because template is a file and
+   return render(request,'home.html',{'name':'Tom'}) # render is used to call html page because we need to render the template because template is a file and
                             # it may have some dynamic content
