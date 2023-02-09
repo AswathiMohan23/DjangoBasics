837daf7a38aa82fa545ce368e4332eed7902088b
AswathiMohan23 <achu999here@gmail.com>
2023-02-09 19:11:01 +0530

    [ASWATHI] ADDED :  code to create models

--- calc/models.py
+++ calc/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class AllCourses(models.Model):
+    coursename=models.CharField(max_length=200)
+    instructorname=models.CharField(max_length=100)
+
+class details(models.Model):
+    course=models.ForeignKey(AllCourses,on_delete=models.CASCADE)
+    selfStudy = models.CharField(max_length = 500)
+    instructorSupported = models.CharField(max_length=500)
+
+
+
