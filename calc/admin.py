81c530899eb4da3f55b73793881010fcc88be844
AswathiMohan23 <achu999here@gmail.com>
2023-02-10 09:40:20 +0530

    [ASWATHI] ADDED . code to connect models to admin pannel

--- calc/admin.py
+++ calc/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import AllCourses
+from .models import Details
+
 
 # Register your models here.
+
+
+admin.site.register(AllCourses)
+admin.site.register(Details)
+
+
--- calc/models.py
+++ calc/models.py
@@ -5,11 +5,30 @@ from django.db import models
 class AllCourses(models.Model):
     coursename=models.CharField(max_length=200)
     instructorname=models.CharField(max_length=100)
+    instructor_contact=models.CharField(max_length=13,verbose_name="phone",default="Not Available")
 
-class details(models.Model):
+
+    def __str__(self):
+        return self.coursename
+
+    class Meta:
+        verbose_name = 'Allcourse'
+        verbose_name_plural = 'Allcourses'
+
+
+class Details(models.Model):
     course=models.ForeignKey(AllCourses,on_delete=models.CASCADE)
-    selfStudy = models.CharField(max_length = 500)
-    instructorSupported = models.CharField(max_length=500)
+    no_of_books = models.CharField(max_length = 100,default="Enter here")
+    no_of_pages = models.CharField(max_length=1000,default="Enter here")
+
+# if we don't give meta class then in amin page it will have one more "s" for our class name eg: Allcoursess instead of Allcourses
+
+    def __str__(self):
+        return self.course
+
+    class Meta:
+        verbose_name = 'Details'
+        verbose_name_plural = 'Details'
 
 
 
